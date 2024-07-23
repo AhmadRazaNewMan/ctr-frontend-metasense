cdca0229226487a3f59d5eae705c1fc58164beb4
AhmadRazaNewMan <webdevolopmentbyar@gmail.com>
2024-07-23 10:18:31 +0500

    adding new connection

---
+++ src/redux/constants/constants.js
@@ -0,0 +1,5 @@
+// export const BASE_URL = "https://cti-backend-361acb35e709.herokuapp.com";
+// export const BASE_URL = "http://localhost:5000";
+ export const BASE_URL = "https://ctr-backend-metasense.vercel.app"
+
+
